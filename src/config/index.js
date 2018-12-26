8e9d83d68ac55e47b48dbc9ca1fcb7a22950ddc7
Bernat <bernatfortet@gmail.com>
2018-12-26 10:37:36 -0500

    Improve meta title and description

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export default {
   bookingsUrl: 'https://handsomehome.youcanbook.me/',
-  webTitle: 'Handsome Home Organization',
-  webDescription: 'Customized organizational systems to support your daily routines.',
+  webTitle: 'Handsome Home Organization - Support your daily routines.',
+  webDescription: 'Clear out the clutter and noise of brands and frills that vye for your attention. Make a home for what serves what’s most important to you. When everything has a place, you can easily reset any mess.',
 }
