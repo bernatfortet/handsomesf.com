a30d7f7cf5d236ee09c56490622d5846dac2e930
Bernat <bernatfortet@gmail.com>
2019-04-26 12:14:30 -0700

    Fix bookings Url

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export default {
-  bookingsUrl: 'https://handsomehome.youcanbook.me/',
-  webTitle: 'Handsome Home Organization - Support your daily routines.',
+  bookingsUrl: 'https://handsomesf.youcanbook.me/',
+  webTitle: 'Handsome - Set up your home and office with supplies curated for form, function, and sustainability',
   webDescription: 'Clear out the clutter and noise of brands and frills that vye for your attention. Make a home that serves what’s most important to you. When everything has a place, you can easily reset any mess.',
 }
