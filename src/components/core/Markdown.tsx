0683f8e762350717a44aa56497993b6606e91942
Bernat <bernatfortet@gmail.com>
2019-07-25 11:15:41 -0700

    Fix li

--- src/components/core/Markdown.tsx
+++ src/components/core/Markdown.tsx
@@ -20,6 +20,9 @@ export default Markdown
 
 
 const StyledMarkdown = styled(ReactMarkdown)`
-  & p{ ${m.tBody} margin-top:0; }
-  & a{ ${s.anchor} }
+  p{ ${m.tBody} margin-top:0; }
+  a{ ${s.anchor} }
+  ul{ padding-left:20px; }
+  li{ ${m.tBody} margin:4px 0; }
+
 `
